e6e4c4b6ffb827daaa0c5663f2d3aaad99d31f4a
RonanFTW <116669207+RonanFTW@users.noreply.github.com>
2024-11-15 19:28:02 -0700

    Update TaskDisplay.js
    improved error handling

--- frontend/src/components/TaskDisplay.js
+++ frontend/src/components/TaskDisplay.js
@@ -3,7 +3,11 @@ import { DelTask } from '../api/Api';
 
 const TaskDisplay = ({task, edit}) => {
     const DEL = async () => {
-        await DelTask(task.id);
+        try {
+            await DelTask(task.id);
+        } catch (err) {
+            console.log("Delete function went boom", err)
+        }
     };
 
     return (
